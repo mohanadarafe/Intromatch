32fcdb92b6aef33c00ef6edbc97e9a1d3b170ec4
Badreddine Loulidi <badred123456@gmail.com>
2020-01-25 21:19:09 -0500

    added dummy messages in the message page

---
+++ constants/Messages.js
@@ -0,0 +1,14 @@
+import shuffleArray from '../utils/shuffleArray'
+
+export const Messages = shuffleArray([
+  {
+    pic: require('../assets/images/blurred/user3_blurred.png'),
+    title: 'Roula, 23',
+    message: "Let's get to your favorite restaurant.",
+  },
+  {
+    pic: require('../assets/images/blurred/user4_blurred.png'),
+    title: 'Fatima, 19',
+    message: "What's the best way to win you over?",
+  },
+])
